74f2a0835320df4169233c636bf69dd54355f6cc
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:28:50 +0530

    taking input from the user

--- src/stage2/Stage2.java
+++ src/stage2/Stage2.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Stage2 {
     public void guessAge() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
     }
 
