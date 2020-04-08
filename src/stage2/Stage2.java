5b88e3ad3658dea0b443b6d0449e81e75e25d623
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:30:24 +0530

    guessAge method created

--- src/stage2/Stage2.java
+++ src/stage2/Stage2.java
@@ -10,6 +10,9 @@ public class Stage2 {
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
 
     }
 
