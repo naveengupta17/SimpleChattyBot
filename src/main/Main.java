c11800117a486665db4c437f20907604cd25a094
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:43:37 +0530

    importing Stage3 class in Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import stage1.Stage1;
 import stage2.Stage2;
+import stage3.Stage3;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Main {
         stage1.greet("Nexa", 2020);
         Stage2 stage2 = new Stage2();
         stage2.guessAge();
+        Stage3 stage3 = new Stage3();
 
 
     }
