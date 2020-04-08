9bb51e6d1beb85e0a11c2fb4d09e770368f9f7bf
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:40:36 +0530

    create method countNumber

--- src/stage3/Stage3.java
+++ src/stage3/Stage3.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Stage3 {
     public void countNumber() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
 
+
+        }
 
     }
-
 }
