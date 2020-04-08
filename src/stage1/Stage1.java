4256876f9bfd7f103565883e2076e26298776f13
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 15:03:04 +0530

    importing Scanner class in Stage1 class

--- src/stage1/Stage1.java
+++ src/stage1/Stage1.java
@@ -1,10 +1,14 @@
 package stage1;
 
+import java.util.Scanner;
+
 public class Stage1 {
     public void greet(String assistantName, int birthYear) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+
 
     }
 
