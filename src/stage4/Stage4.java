e4aa7974901a3db8044a35e32096ee9c72fbd51a
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:49:35 +0530

    writing the question for test in test Method

--- src/stage4/Stage4.java
+++ src/stage4/Stage4.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Stage4 {
     public void test() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+
+
     }
 
 }
