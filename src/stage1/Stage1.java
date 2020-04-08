e9f382ec008816784fde8bd8094de5bbddca91ec
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:16:26 +0530

    greet method created

--- src/stage1/Stage1.java
+++ src/stage1/Stage1.java
@@ -1,7 +1,10 @@
 package stage1;
 
 public class Stage1 {
-    public void greet(String assisnatName, int birthYear) {
+    public void greet(String assistantName, int birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
 
     }
 
