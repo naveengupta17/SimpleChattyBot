65e65a05c11568de419e30df0fdad68d24ab30c2
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 14:31:34 +0530

    importing Stage2 class in main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
 import stage1.Stage1;
+import stage2.Stage2;
 
 public class Main {
     public static void main(String[] args) {
         Stage1 stage1 = new Stage1();
         stage1.greet("Nexa", 2020);
+        Stage2 stage2 = new Stage2();
 
     }
 
