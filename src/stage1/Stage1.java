0c3b9b4421a17dc6edefddefe1faa0b9b19fe4c2
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-04-08 15:04:31 +0530

    greet method created

--- src/stage1/Stage1.java
+++ src/stage1/Stage1.java
@@ -8,6 +8,7 @@ public class Stage1 {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
 
 
     }
